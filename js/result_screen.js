1ca30cc089ee6ab7b4e0c4e73f909ab9697ada38
yoshipc <yoooo@yoshipc.net>
2022-04-25 15:54:44 +0900

    各種検索エンジンの対応
    Google, Yahoo!, Bingの3種類に対応するように暫定的に変更。

--- js/result_screen.js
+++ js/result_screen.js
@@ -1,24 +1,42 @@
 $(document).ready(function(){
-    $('div#search a').on('click', function(e){
-        // ページの遷移を抑止
-        e.preventDefault();
-        const ROOT = 'div#rso > div';
-        let s_query = $('input').val();
-        let s_order = -1;
-        let href_url = $(this).attr('href');
-        let parent = $(ROOT).children();
-        let params = new URLSearchParams(location.href);
-        let offset = params.get('start');
-        if (offset === null) offset = 0
-        else offset = parseInt(offset);
+    if(location.hostname.match(/google/)) {
+        onGoogle();
+    } else if(location.hostname.match(/yahoo/)) {
+        onYahoo();
+    } else if(location.hostname.match(/bing/)) {
+        onBing();
+    }
 
-        // 親要素をindex()が-1でなくなるところまで遡る
-        let temp = $(this);
-        do {
-            temp = temp.parent();
-            s_order = parent.index(temp);
-        } while(temp !== null && s_order === -1)
-        
-        console.log('検索クエリ:' + s_query + ', リンク先:' + href_url + ', 順位:' + (offset + s_order));
-    });
+    function onGoogle() {
+        $('div#search a').on('click', function(e){
+            // ページの遷移を抑止
+            e.preventDefault();
+            const ROOT = 'div#rso > div';
+            let s_query = $('input').val();
+            let s_order = -1;
+            let href_url = $(this).attr('href');
+            let parent = $(ROOT).children();
+            let params = new URLSearchParams(location.href);
+            let offset = params.get('start');
+            if (offset === null) offset = 0
+            else offset = parseInt(offset);
+    
+            // 親要素をindex()が-1でなくなるところまで遡る
+            let temp = $(this);
+            do {
+                temp = temp.parent();
+                s_order = parent.index(temp);
+            } while(temp !== null && s_order === -1)
+            
+            console.log('検索クエリ:' + s_query + ', リンク先:' + href_url + ', 順位:' + (offset + s_order));
+        });
+    }
+
+    function onYahoo() {
+
+    }
+
+    function onBing() {
+
+    }
 });
