ffba3c9734716006c1bb6645c857bc4a346fd374
yoshipc <yoooo@yoshipc.net>
2022-04-25 18:23:34 +0900

    各種検索エンジンに暫定対応
    3大エンジンに対応するように、result_screen.jsの中身を関数化してスマートにするなど。

--- js/result_screen.js
+++ js/result_screen.js
@@ -8,35 +8,55 @@ $(document).ready(function(){
     }
 
     function onGoogle() {
-        $('div#search a').on('click', function(e){
+        $('div#search a').on('click', function(e) {
             // ページの遷移を抑止
             e.preventDefault();
             const ROOT = 'div#rso > div';
             let s_query = $('input').val();
-            let s_order = -1;
             let href_url = $(this).attr('href');
-            let parent = $(ROOT).children();
-            let params = new URLSearchParams(location.href);
-            let offset = params.get('start');
-            if (offset === null) offset = 0
-            else offset = parseInt(offset);
-    
-            // 親要素をindex()が-1でなくなるところまで遡る
-            let temp = $(this);
-            do {
-                temp = temp.parent();
-                s_order = parent.index(temp);
-            } while(temp !== null && s_order === -1)
             
-            console.log('検索クエリ:' + s_query + ', リンク先:' + href_url + ', 順位:' + (offset + s_order));
+            console.log('検索クエリ:' + s_query + ', リンク先:' + href_url + ', 順位:' + (get_param_int(location.href, 'start') + scan_order(ROOT, $(this))));
         });
     }
 
     function onYahoo() {
+        $('div.sw-Card__title a').on('click', function(e) {
+            // ページの遷移を抑止
+            e.preventDefault();
+            const ROOT = '.Contents__innerGroupBody';
+            let s_query = $('input').val();
+            let href_url = $(this).attr('href');
 
+            console.log('検索クエリ:' + s_query + ', リンク先:' + href_url + ', 順位:' + (get_param_int(location.href, 'b') + scan_order(ROOT, $(this))));
+        })
     }
 
     function onBing() {
+        $('div.b_title a').on('click', function(e) {
+            // ページの遷移を抑止
+            e.preventDefault();
+            const ROOT = 'ol#b_results';
+            let s_query = $('input').val();
+            let href_url = $(this).attr('href');
 
+            console.log('検索クエリ:' + s_query + ', リンク先:' + href_url + ', 順位:' + (get_param_int(location.href, 'first') + scan_order(ROOT, $(this))));
+        });
+    }
+
+    function scan_order(root, temp) {
+        let parent = $(root).children();
+        let s_order = -1;
+        do {
+            temp = temp.parent();
+            s_order = parent.index(temp);
+        } while(temp !== null && s_order === -1)
+
+        return s_order;
+    }
+
+    function get_param_int(url, key) {
+        let value = new URLSearchParams(url).get(key);
+        if (value === null) return 0;
+        else return parseInt(value);
     }
 });
